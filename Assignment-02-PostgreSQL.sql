8b7102b763663355a51520ff644324e5bf07522b
Prosenjit Pal <iprosenjitp@yahoo.com>
2025-05-24 20:25:11 +0600

    Add solution to all except problem 08

--- Assignment-02-PostgreSQL.sql
+++ Assignment-02-PostgreSQL.sql
@@ -44,4 +44,63 @@ SELECT * FROM rangers;
 SELECT * FROM species;
 SELECT * FROM sightings;
 
+-- Problem 01
+INSERT INTO rangers (name, region) VALUES
+('Derek Fox', 'Coastal Plains');
 
+-- Problem 02
+SELECT COUNT(DISTINCT species_id) AS unique_species_count 
+FROM sightings;
+
+-- Problem 03
+SELECT *
+FROM sightings
+WHERE location LIKE '%Pass%';
+
+-- Problem 04
+SELECT r.name,
+       COUNT(s.sighting_id) AS total_sightings
+FROM rangers AS r INNER JOIN 
+     sightings AS s 
+USING(ranger_id)
+GROUP BY r.name
+ORDER BY r.name ASC;
+
+-- Problem 05
+SELECT s.common_name
+FROM species AS s LEFT JOIN 
+     sightings AS si
+USING(species_id)
+WHERE si.sighting_id IS NULL;
+
+-- Problem 06
+SELECT s.common_name,
+       si.sighting_time,
+       r.name   
+FROM sightings AS si INNER JOIN 
+     species AS s USING(species_id) INNER JOIN
+     rangers AS r USING(ranger_id)
+ORDER BY sighting_time DESC
+LIMIT 2;
+
+-- Problem 07
+UPDATE species
+SET conservation_status = 'Historic'
+WHERE discovery_date < '1800-01-01';
+
+-- Problem 08 - Label each sighting's time of day as 'Morning', 'Afternoon', or 'Evening'.
+-- Morning: before 12 PM
+-- Afternoon: 12 PM–5 PM
+-- Evening: after 5 PM
+
+
+-- Problem 09
+DELETE
+FROM rangers AS r 
+WHERE r.ranger_id IN (
+    SELECT ranger_id 
+    FROM rangers AS r LEFT JOIN 
+         sightings AS s
+    USING(ranger_id)
+    WHERE s.sighting_id IS NULL
+);
